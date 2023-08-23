6577bad6762a06786407c505d51e23f228fa2cdb
İsmail Yarar <ismailyarar4@gmail.com>
2023-08-23 22:44:52 +0300

    Add og image route. Add metadata to home page and summary page.

--- src/MetadataDefault.ts
+++
@@ -1,4 +0,0 @@
-export const title = 'SummariX';
-export const description =
-  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ex doloremque qui asperiores iure. Nesciunt!';
-export const keywords = ['keyword1', 'keyword2', 'keyword3'];
