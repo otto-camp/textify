a3acb8241a3e28d5f71e33701ba0ba7333d97d3a
otto-camp <ismailyarar4@gmail.com>
2023-11-20 17:59:26 +0300

    Fix pathname error in sitemap.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ import { env } from '@/env.mjs';
 import { type MetadataRoute } from 'next';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes = ['', '/tools', '/ocr', '/sentiment', '/summary'].map(
+  const routes = ['', '/tools', '/tools/ocr', '/tools/sentiment', '/tools/summary'].map(
     (url) => ({
       url: `${env.NEXT_PUBLIC_APP_URL}/${url}`,
       lastModified: new Date().toISOString(),
