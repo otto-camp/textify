5eb86233b51f21e4bad37ddfd20d8c3a4809bbd9
otto-camp <ismailyarar4@gmail.com>
2023-12-06 17:34:13 +0300

    Add Loglib analytics.

--- src/components/layouts/Analytics.tsx
+++ src/components/layouts/Analytics.tsx
@@ -1,8 +1,12 @@
 import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
+import Loglib from "@loglib/tracker/react"
 export default function Analytics() {
   return (
     <>
       <VercelAnalytics />
+      <Loglib config={{
+        id:'textify_yarar'
+      }}/>
     </>
   );
 }
