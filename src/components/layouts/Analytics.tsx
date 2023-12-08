3bf19dc2601352c54825c72ec68b0e56e58ba4ac
otto-camp <ismailyarar4@gmail.com>
2023-12-08 19:09:05 +0300

    Add SpeedInsights.

--- src/components/layouts/Analytics.tsx
+++ src/components/layouts/Analytics.tsx
@@ -1,12 +1,16 @@
 import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
-import Loglib from "@loglib/tracker/react"
+import Loglib from '@loglib/tracker/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 export default function Analytics() {
   return (
     <>
       <VercelAnalytics />
-      <Loglib config={{
-        id:'textify_yarar'
-      }}/>
+      <Loglib
+        config={{
+          id: 'textify_yarar',
+        }}
+      />
+      <SpeedInsights />
     </>
   );
 }
